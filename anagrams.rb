e3804613e109dd9c1a3cffbc477fc1a5612c6c11
Torben Ginsberg <tginsberg8@gmail.com>
2022-06-17 15:11:35 -0400

    completed anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -8,4 +8,40 @@ def first_anagram?(str1, str2)
     false
 end
 
-p first_anagram?("cat", "art")
+
+def second_anagram?(str1,str2)
+    str1.each_char do |ele|
+        idx = str2.index(ele)
+        return false if idx == nil
+        str2.slice!(idx)
+    end
+    if str2 != ""
+        return false
+    end
+    true
+end
+
+
+def third_anagram?(str1, str2)
+    str1.chars.sort_by(&:downcase).join == str2.chars.sort_by(&:downcase).join
+end
+
+def fourth_anagram?(str1, str2)
+    str_hash = Hash.new(0)
+
+    str1.each_char do |ele|
+        str_hash[ele] += 1
+    end
+
+    str2.each_char do |ele|
+        str_hash[ele] -= 1
+    end
+
+    str_hash.each do |key, val|
+        if val != 0
+            return false
+        end
+    end
+
+    true
+end
